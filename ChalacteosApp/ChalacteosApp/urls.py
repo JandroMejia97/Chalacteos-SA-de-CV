c39f1ecf53758f3e840cf8ebb071a31ba552756f
luismy18 <44522879+luismy18@users.noreply.github.com>
2018-11-03 06:01:26 -0600

    Update urls.py

--- ChalacteosApp/ChalacteosApp/urls.py
+++ ChalacteosApp/ChalacteosApp/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('apps.contabilidad.urls')),
+    path('contabilidad/',include('apps.contabilidad.urls')),
 ]
