868ee0fd5a41748ae7d19b4f87d61702ecc09c1b
luismy18 <44522879+luismy18@users.noreply.github.com>
2018-11-03 06:02:52 -0600

    Update urls.py

--- ChalacteosApp/apps/contabilidad/urls.py
+++ ChalacteosApp/apps/contabilidad/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
+from django.conf.urls import url,include
+from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import views as auth_views
 from . import views
 
+app_name ='contabilidad'
+
 urlpatterns = [
-	path('',views.post_list,name='post_list'),
+	url(r'^cuentas/$', auth_views.LoginView.as_view(
+            template_name= 'contabilidad/cuentas.html'),
+            name='cuentas' ),
+
 ]
